efa454864e3144a806949d34be8121f23aba98f7
Marvin Carrasco <marccenvivo@gmail.com>
2020-05-19 16:36:50 -0500

    updating data table Games

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,9 +17,21 @@ g.release_date = '2019-09-13'
 g.save
 =end
 
+=begin
 Game.create([ {:name=>"Zombie Driver: Immortal Edition", :genre=>"Racing", :price=>5999, :release_date=>"2019-Jul-25"},
 {:name=>"Remnant: From the Ashes", :genre=>"Shooter", :price=>6999, :release_date=>"2019-Aug-20"},
 {:name=>"Monster Hunter: World - Iceborne", :genre=>"Adventure", :price=>6999, :release_date=>"2019-Sep-06"},
 {:name=>"Cyberpunk 2077", :genre=>"Role-playing (RPG)", :price=>4999, :release_date=>"2020-Sep-17"},
 {:name=>"Pokémon Shield", :genre=>"Role-playing (RPG)", :price=>5999, :release_date=>"2019-Nov-15"},
 {:name=>"Children of Morta", :genre=>"Role-playing (RPG)", :price=>6999, :release_date=>"2019-Sep-03"} ])
+=end
+
+#g = Game.find_by(name: 'Borderlands 3')
+#g.update(genre: 'Shooter')
+#g.save
+
+#g = Game.find_by(name: 'Pokémon Shield')
+#g.update(price: 3999)
+
+g = Game.where(genre: "Role-playing (RPG)")
+g.update(price: 299)
