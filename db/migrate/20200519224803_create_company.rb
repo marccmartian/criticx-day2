a1f70f1e5c003a2afdddd4f5d24ad8a1dd42ae19
Marvin Carrasco <marccenvivo@gmail.com>
2020-05-19 18:31:05 -0500

    create Company entity

---
+++ db/migrate/20200519224803_create_company.rb
@@ -0,0 +1,10 @@
+class CreateCompany < ActiveRecord::Migration[6.0]
+  def change
+    create_table :companies do |t|
+      t.string :name
+      t.text :description
+      t.date :start_date
+      t.string :country
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_19_162754) do
+ActiveRecord::Schema.define(version: 2020_05_19_224803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.date "start_date"
+    t.string "country"
+  end
 
   create_table "games", force: :cascade do |t|
     t.string "name"
