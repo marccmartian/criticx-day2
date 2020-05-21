fdacc96d082fdaeb88d98e942bc1625ba146c745
Marvin Carrasco <marccenvivo@gmail.com>
2020-05-20 22:28:51 -0500

    create a new game and company from insonmia

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -8,4 +8,18 @@ class CompaniesController < ApplicationController
     @companies = Company.find(params[:id])
     render json: @companies
   end
+
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      render json: @company
+    else
+      render json: @company.errors
+    end
+  end
+
+  private
+  def company_params
+    params.require(:company).permit(:name, :description, :start_date, :country)
+  end
 end
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,4 +8,18 @@ class GamesController < ApplicationController
     @game = Game.find(params[:id])
     render json: @game
   end
+
+  def create
+    @game = Game.new(game_params)
+    if @game.save
+      render json: @game
+    else
+      render json: @game.errors
+    end
+  end
+
+  private
+  def game_params
+    params.require(:game).permit(:name, :genre, :price, :release_date, :company_id)
+  end
 end
